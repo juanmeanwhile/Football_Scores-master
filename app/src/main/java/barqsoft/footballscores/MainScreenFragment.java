42a056711fc6bcd1505e49b4f0407a1160b65221
juanmeanwhile <juanmeanwhile@gmail.com>
2015-09-24 17:25:34 +0200

    Initial commit

---
+++ app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -0,0 +1,137 @@
+package barqsoft.footballscores;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import barqsoft.footballscores.service.myFetchService;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainScreenFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    public ScoresAdapter mAdapter;
+    private ListView mList;
+    public static final int SCORES_LOADER = 0;
+    private String[] fragmentdate = new String[1];
+    private int mLastSelectedItem = -1;
+    private int mSelectedMatchId;
+
+    private OnFragmentInteractionListener mListener;
+
+    public interface OnFragmentInteractionListener {
+        void onMatchSelected(int id);
+    }
+
+    public MainScreenFragment() {
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.getPackageName() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+
+    private void updateScores()
+    {
+        Intent service_start = new Intent(getActivity(), myFetchService.class);
+        getActivity().startService(service_start);
+    }
+    public void setFragmentDate(String date)
+    {
+        fragmentdate[0] = date;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             final Bundle savedInstanceState) {
+        updateScores();
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mList = (ListView) rootView.findViewById(R.id.scores_list);
+        mList.setItemsCanFocus(true);
+        mAdapter = new ScoresAdapter(getActivity(),null,0);
+        mList.setAdapter(mAdapter);
+        getLoaderManager().initLoader(SCORES_LOADER, null, this);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ViewHolder selected = (ViewHolder) view.getTag();
+                mAdapter.setDetailId(selected.match_id);
+                mListener.onMatchSelected( (int) selected.match_id);
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        mList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mAdapter.setDetailId(id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
+    {
+        return new CursorLoader(getActivity(),DatabaseContract.scores_table.buildScoreWithDate(),
+                null,null,fragmentdate,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
+    {
+        //Log.v(FetchScoreTask.LOG_TAG,"loader finished");
+        //cursor.moveToFirst();
+        /*
+        while (!cursor.isAfterLast())
+        {
+            Log.v(FetchScoreTask.LOG_TAG,cursor.getString(1));
+            cursor.moveToNext();
+        }
+        */
+
+        int i = 0;
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast())
+        {
+            i++;
+            cursor.moveToNext();
+        }
+        //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
+        mAdapter.swapCursor(cursor);
+        //mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader)
+    {
+        mAdapter.swapCursor(null);
+    }
+
+
+}
